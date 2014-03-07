06a997a7908671135972eadc36fcf27a2e8b4e2a
lxy <lgjd33402@gmail.com>
2014-03-07 19:05:26 +0800

    add prefix

---
+++ PixePixie/PixePixie/PPAppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.m
+//  PixePixie
+//
+//  Created by silver6wings on 14-3-5.
+//  Copyright (c) 2014年 Psyches. All rights reserved.
+//
+
+#import "PPAppDelegate.h"
+#import "ViewController.h"
+
+@implementation PPAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
+    
+    ViewController * viewController = [[ViewController alloc] init];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = viewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+							
+- (void)applicationWillResignActive:(UIApplication *)application{}
+- (void)applicationDidEnterBackground:(UIApplication *)application{}
+- (void)applicationWillEnterForeground:(UIApplication *)application{}
+- (void)applicationDidBecomeActive:(UIApplication *)application{}
+- (void)applicationWillTerminate:(UIApplication *)application{}
+
+@end
