e015dcf4516c2171616d545da5cf9891d377c8cb
lxy <lgjd33402@gmail.com>
2014-03-09 11:21:52 +0800

    add coveflowview

---
+++ PixelPixie/PixePixie/Views/PPCoverflow.h
@@ -0,0 +1,11 @@
+//
+//  PPCoverflow.h
+//  PixelPixie
+//
+//  Created by liuxiaoyu on 14-3-9.
+//  Copyright (c) 2014年 Psyches. All rights reserved.
+//
+
+@interface PPCoverflow : UIView
+
+@end
---
+++ PixelPixie/PixePixie/Views/PPCoverflow.m
@@ -0,0 +1,22 @@
+//
+//  PPCoverflow.m
+//  PixelPixie
+//
+//  Created by liuxiaoyu on 14-3-9.
+//  Copyright (c) 2014年 Psyches. All rights reserved.
+//
+
+#import "PPCoverflow.h"
+
+@implementation PPCoverflow
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+		
+    }
+    return self;
+}
+
+@end
