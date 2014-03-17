253d111b8442b52d870845879503df635da3d435
lxy <lgjd33402@gmail.com>
2014-03-17 16:37:13 +0800

    add TabBarController

---
+++ PixelPixie/PixePixie/Controllers/PPTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  PPTabBarController.h
+//  PixelPixie
+//
+//  Created by liuxiaoyu on 14-3-17.
+//  Copyright (c) 2014年 Psyches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPTabBarController : UIViewController
+
+@end
---
+++ PixelPixie/PixePixie/Controllers/PPTabBarController.m
@@ -0,0 +1,36 @@
+//
+//  PPTabBarController.m
+//  PixelPixie
+//
+//  Created by liuxiaoyu on 14-3-17.
+//  Copyright (c) 2014年 Psyches. All rights reserved.
+//
+
+#import "PPTabBarController.h"
+
+@interface PPTabBarController ()
+
+@end
+
+@implementation PPTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+		
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
