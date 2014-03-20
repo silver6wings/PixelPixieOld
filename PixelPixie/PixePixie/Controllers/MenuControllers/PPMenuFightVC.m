c9e7a89017ddacf3c2b418793614b09965a17a47
lxy <lgjd33402@gmail.com>
2014-03-20 18:04:40 +0800

    add MenuVCs

---
+++ PixelPixie/PixePixie/Controllers/MenuControllers/PPMenuFightVC.m
@@ -0,0 +1,36 @@
+//
+//  PPMenuFightVC.m
+//  PixelPixie
+//
+//  Created by liuxiaoyu on 14-3-20.
+//  Copyright (c) 2014年 Psyches. All rights reserved.
+//
+
+#import "PPMenuFightVC.h"
+
+@interface PPMenuFightVC ()
+
+@end
+
+@implementation PPMenuFightVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+		
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
---
+++ PixelPixie/PixePixie/Controllers/MenuControllers/PPMenuVC.h
@@ -0,0 +1,18 @@
+//
+//  PPMenuVC.h
+//  PixelPixie
+//
+//  Created by liuxiaoyu on 14-3-20.
+//  Copyright (c) 2014年 Psyches. All rights reserved.
+//
+
+
+@interface PPMenuVC : UIViewController
+
+@property (nonatomic, copy) NSString *menuTitle;
+
+@property (nonatomic, readonly) PPMenuSwitchType type;
+
+@property (nonatomic, copy) void (^removeFromParentVCBlock)(UIViewController *childVC);
+
+@end
