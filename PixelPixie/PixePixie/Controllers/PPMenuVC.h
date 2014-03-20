abb163e19eaf768c6d70788f53702ab641d5906f
lxy <lgjd33402@gmail.com>
2014-03-20 10:37:29 +0800

    add MenuVC

---
+++ PixelPixie/PixePixie/Controllers/PPMenuVC.h
@@ -0,0 +1,11 @@
+//
+//  PPMenuVC.h
+//  PixelPixie
+//
+//  Created by liuxiaoyu on 14-3-20.
+//  Copyright (c) 2014年 Psyches. All rights reserved.
+//
+
+@interface PPMenuVC : UIViewController
+
+@end
---
+++ PixelPixie/PixePixie/Controllers/PPMenuVC.m
@@ -0,0 +1,36 @@
+//
+//  PPMenuVC.m
+//  PixelPixie
+//
+//  Created by liuxiaoyu on 14-3-20.
+//  Copyright (c) 2014年 Psyches. All rights reserved.
+//
+
+#import "PPMenuVC.h"
+
+@interface PPMenuVC ()
+
+@end
+
+@implementation PPMenuVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+		
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
