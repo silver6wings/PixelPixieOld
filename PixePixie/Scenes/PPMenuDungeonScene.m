9b00a543111dbde60afbd6442a0e54a6a843f663
silver6wings <silver6wings@gmail.com>
2014-10-15 15:05:57 +0800

    添加了一个场景贴图

--- PixePixie/Scenes/PPMenuDungeonScene.m
+++ PixePixie/Scenes/PPMenuDungeonScene.m
@@ -11,7 +11,8 @@
     if (self = [super initWithSize:size]) {
         [self setBackTitleText:@"小场景" andPositionY:450.0f];
         [self setBackgroundColor:[UIColor purpleColor]];
-        
+        NSString * mapName = [NSString stringWithFormat:@"map_scene_%@.png", kPPElementTypeString[elementType]];
+        [self addChild:[SKSpriteNode spriteNodeWithImageNamed:mapName]];
         [self addPassChoose];
     }
     return self;
--- PixePixie/Scenes/PPMenuThemeScene.m
+++ PixePixie/Scenes/PPMenuThemeScene.m
@@ -10,40 +10,38 @@
         self.backgroundColor = [UIColor blueColor];
         [self setBackTitleText:@"世界地图" andPositionY:450.0f];
         
-        
         for (int i = 0; i < 3; i++) {
             
-          PPSpriteButton *  passButton = [PPSpriteButton buttonWithColor:[UIColor orangeColor] andSize:CGSizeMake(80, 80)];
-            
+            PPSpriteButton *  passButton = [PPSpriteButton buttonWithColor:[UIColor orangeColor] andSize:CGSizeMake(80, 80)];
             switch (i) {
                 case 0:
                 {
-                    [passButton setLabelWithText:[NSString stringWithFormat:@"场景 %d",i+1] andFont:[UIFont systemFontOfSize:15] withColor:nil];
+                    [passButton setLabelWithText:[NSString stringWithFormat:@"场景 %d",i+1]
+                                         andFont:[UIFont systemFontOfSize:15] withColor:nil];
                     passButton.position = CGPointMake(80.0f,80.0f);
                 }
                     break;
                 case 1:
                 {
-                    [passButton setLabelWithText:[NSString stringWithFormat:@"场景 %d",i+1] andFont:[UIFont systemFontOfSize:15] withColor:nil];
-                    
+                    [passButton setLabelWithText:[NSString stringWithFormat:@"场景 %d",i+1]
+                                         andFont:[UIFont systemFontOfSize:15] withColor:nil];
                     passButton.position = CGPointMake(120.0f,380.0f);
                 }
-                    
                     break;
                 case 2:
                 {
-                    [passButton setLabelWithText:[NSString stringWithFormat:@"场景 %d",i+1] andFont:[UIFont systemFontOfSize:15] withColor:nil];
-                    
+                    [passButton setLabelWithText:[NSString stringWithFormat:@"场景 %d",i+1]
+                                         andFont:[UIFont systemFontOfSize:15] withColor:nil];
                     passButton.position = CGPointMake(220.0f,180.0f);
                 }
-                    
                     break;
-                    
                 default:
                     break;
             }
-            passButton.name = [NSString stringWithFormat:@"%d",i+PP_PASSNUM_CHOOSE_TABLE_TAG];
-            [passButton addTarget:self selector:@selector(passChoose:) withObject:passButton.name forControlEvent:PPButtonControlEventTouchUpInside];
+            
+            passButton.name = [NSString stringWithFormat:@"%d", i + PP_PASSNUM_CHOOSE_TABLE_TAG];
+            [passButton addTarget:self selector:@selector(passChoose:)
+                       withObject:passButton.name forControlEvent:PPButtonControlEventTouchUpInside];
             [self addChild:passButton];
         }
     }
@@ -57,20 +55,23 @@
 
 -(void)passChoose:(NSString *)passchoose
 {
-        NSDictionary *dictPassInfo = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"PassInfo"
+        NSDictionary * dictPassInfo = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"PassInfo"
                                                                                                                 ofType:@"plist"]];
-        NSArray *passArray = [[NSArray alloc] initWithArray:[dictPassInfo objectForKey:@"transcriptinfo"]];
+        NSArray * passArray = [[NSArray alloc] initWithArray:[dictPassInfo objectForKey:@"transcriptinfo"]];
         
         NSInteger passCount = [passArray count];
-        NSInteger index = [passchoose integerValue]-PP_PASSNUM_CHOOSE_TABLE_TAG;
-        NSDictionary *passDictInfo = nil;
+        NSInteger index = [passchoose integerValue] - PP_PASSNUM_CHOOSE_TABLE_TAG;
+    
+        NSDictionary * passDictInfo = nil;
         if (passCount > index) {
             passDictInfo=[NSDictionary dictionaryWithDictionary:[passArray objectAtIndex:index]];
         }
         
-        PPMenuDungeonScene * menuDungeonScene = [[PPMenuDungeonScene alloc] initWithSize:self.view.bounds.size];
-        menuDungeonScene.passDictInfo=passDictInfo;
-        menuDungeonScene->previousScene=self;
+        PPMenuDungeonScene * menuDungeonScene = [[PPMenuDungeonScene alloc] initWithSize:self.view.bounds.size
+                                                                              andElement:PPElementTypeFire];
+    
+        menuDungeonScene.passDictInfo = passDictInfo;
+        menuDungeonScene->previousScene = self;
         menuDungeonScene.scaleMode = SKSceneScaleModeAspectFill;
     
         [self.view presentScene:menuDungeonScene];
